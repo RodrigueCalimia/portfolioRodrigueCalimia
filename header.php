91eaa7c12cb66457d4a6f69270962e7a0f9c121b
Rodrigue <easyapps90@gmail.com>
2020-11-23 13:09:04 +0100

    Mise en forme du header
    mise en forme du logo et slogan

--- header.php
+++ header.php
@@ -2,19 +2,25 @@
 <html>
     <head <?php language_attributes(); ?>>
         <meta charset="<?php bloginfo('charset'); ?>"> <title><?php the_title(); ?></title>
-        <link rel="stylesheet" href="<?php
-        bloginfo('stylesheet_url'); ?>" type="text/css">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css"> 
         <?php wp_head(); ?>
     </head>
     <body>
         <div class="wrap"> 
             <header>
-                <h1><a><?php bloginfo('name'); ?></a></h1>
-                <h2><?php bloginfo('description'); ?></h2>
-                <nav class="menu">
-                    <?php wp_nav_menu ( array (
-                        'theme_location' => 'header-menu'
-                        ) ); 
-                    ?>
-                </nav>
+                <div class="container">
+                    <div class="logo">
+                        <h1><a><?php bloginfo('name'); ?></a></h1>
+                        <h2><?php bloginfo('description'); ?></h2>
+                    </div>
+                    <div class="nav-toggle">
+                        <div class="nav-toggle-bar"></div>
+                    </div>
+                    <nav class="menu">
+                        <?php wp_nav_menu ( array (
+                            'theme_location' => 'header-menu'
+                            ) ); 
+                        ?>
+                    </nav>
+                </div>
             </header>

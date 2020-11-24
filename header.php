f2440c1cba812dd985b4c47516c79e3654b73d3c
Rodrigue <easyapps90@gmail.com>
2020-11-24 08:40:49 +0100

    Modification du template header.php
    ajout du ligne de code pour la compatibilité avec les écrans = responsive

--- header.php
+++ header.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head <?php language_attributes(); ?>>
-        <meta charset="<?php bloginfo('charset'); ?>"> <title><?php the_title(); ?></title>
+        <meta 
+            name="viewport" content="width=device-width, initial-scale=1.0"
+            charset="<?php bloginfo('charset'); ?>"
+        >
+        <title><?php the_title(); ?></title>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
         <?php wp_head(); ?>
     </head>
